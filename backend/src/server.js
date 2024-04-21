d0d24e2104b218bf6f19b682afaae5008b908ef5
Praveen Goswami <praveen828589@gmail.com>
2024-04-21 18:54:21 +0530

    Add: Error handler middleware and function

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,3 +36,14 @@ app.use("/api/auth", authRoutes)
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`)
 })
+
+//Error Handler Middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
