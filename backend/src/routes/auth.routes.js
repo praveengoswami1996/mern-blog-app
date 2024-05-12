2ea86b4926b585cf1636f7cf630de000874324a2
Praveen Goswami <praveen828589@gmail.com>
2024-05-12 21:00:26 +0530

    Create: Signin API

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signupController } from "../controllers/auth.controller.js";
+import { signupController, signinController } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/signup", signupController)
+router.post("/signup", signupController);
+router.post("/signin", signinController)
 
 export default router;
